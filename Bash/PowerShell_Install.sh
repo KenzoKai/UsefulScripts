81ff633c8d432c4dad715efacd1220bd6ad88547
KenzoKai <john.bradley25@outlook.com>
2024-01-02 14:03:03 -0600

    pwsh2

--- Bash/PowerShell_Install.sh
+++ Bash/PowerShell_Install.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y software-properties-common
-wget -q https://packages.microsoft.com/config/ubuntu/$DISTRIB_ID/$(${DISTRIB_RELEASE}+0).list
-sudo mv $DISTRIB_ID-$(${DISTRIB_RELEASE}+0).list /etc/apt/sources.list.d/microsoft-prod.list
-sudo apt-get install -y apt-transport-https
-sudo apt-get update -y
-sudo apt-get install -y pwsh
+###################################
+# Prerequisites
+
+# Update the list of packages
+sudo apt-get update
+
+# Install pre-requisite packages.
+sudo apt-get install -y wget apt-transport-https software-properties-common
+
+# Get the version of Ubuntu
+source /etc/os-release
+
+# Download the Microsoft repository keys
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+
+# Register the Microsoft repository keys
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Delete the Microsoft repository keys file
+rm packages-microsoft-prod.deb
+
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
+
+###################################
+# Install PowerShell
+sudo apt-get install -y powershell
