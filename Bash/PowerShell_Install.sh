4896d981b27ac573bd8275f7cf3e506b6f5f0dfe
KenzoKai <john.bradley25@outlook.com>
2024-01-02 14:00:13 -0600

    pwsh

--- Bash/PowerShell_Install.sh
+++ Bash/PowerShell_Install.sh
@@ -5,4 +5,4 @@ wget -q https://packages.microsoft.com/config/ubuntu/$DISTRIB_ID/$(${DISTRIB_REL
 sudo mv $DISTRIB_ID-$(${DISTRIB_RELEASE}+0).list /etc/apt/sources.list.d/microsoft-prod.list
 sudo apt-get install -y apt-transport-https
 sudo apt-get update -y
-sudo apt-get install -y powershell
+sudo apt-get install -y pwsh
