db9b4ee14082eee66db3af1ac30d6064c5b8df03
KenzoKai <john.bradley25@outlook.com>
2024-01-02 13:38:09 -0600

    Add setup script

---
+++ Ubuntu_Setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download and run Docker installation script
+curl -fsSL https://raw.githubusercontent.com/KenzoKai/UsefulScripts/main/Bash/Docker_Install.sh | sudo bash
+
+# Download and run NVM installation script
+curl -fsSL https://raw.githubusercontent.com/KenzoKai/UsefulScripts/main/Bash/NVM_Install.sh | sudo bash
+
+# Download and run Python installation script
+curl -fsSL https://raw.githubusercontent.com/KenzoKai/UsefulScripts/main/Bash/Python_Install.sh | sudo bash
