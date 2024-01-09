333a472f316fd1f7038a96b3156e20d78092f324
John Bradley <john.bradley25@outlook.com>
2024-01-08 21:59:50 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 echo "Setup Script Initializing..."
 bash -c "curl -fsSL -o Ubuntu_Setup.sh https://raw.githubusercontent.com/KenzoKai/UsefulScripts/main/Ubuntu_Setup.sh"
 bash -c "chmod +x Ubuntu_Setup.sh"
-bash -c "./Ubuntu_Setup.sh"
+bash -i "./Ubuntu_Setup.sh"
