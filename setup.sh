5d4bd0c43f6fb30078ce31b6623b0ed05a4aabd7
John Bradley <john.bradley25@outlook.com>
2024-01-08 21:55:25 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+Echo "Setup Script Initializing..."
+curl -fsSL -o Ubuntu_Setup.sh https://raw.githubusercontent.com/KenzoKai/UsefulScripts/main/Ubuntu_Setup.sh
+chmod +x Ubuntu_Setup.sh
+./Ubuntu_Setup.sh
